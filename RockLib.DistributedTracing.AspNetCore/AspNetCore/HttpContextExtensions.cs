80875da7805c9494733dd73b6427306b1ce0b397
jmckain <JamesMcKain@quickenloans.com>
2021-01-20 16:21:46 -0500

    concept of traceId aligning

--- RockLib.DistributedTracing.AspNetCore/AspNetCore/HttpContextExtensions.cs
+++ RockLib.DistributedTracing.AspNetCore/AspNetCore/HttpContextExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Primitives;
+using OpenTelemetry.Trace;
 using System;
 
 namespace RockLib.DistributedTracing.AspNetCore
@@ -50,6 +51,13 @@ namespace RockLib.DistributedTracing.AspNetCore
                 if (httpContext.GetHeaderValue(correlationIdHeader) is StringValues correlationId && correlationId.Count > 0)
                     accessor.CorrelationId = correlationId;
 
+                // lean on OpenTelemetry for the traceId
+                if (accessor.CorrelationId is null)
+                {
+                    accessor.CorrelationId = Tracer.CurrentSpan?.Context.TraceId.ToString();
+                }
+
+                // fallback to creating a new value
                 if (accessor.CorrelationId is null)
                     accessor.CorrelationId = Guid.NewGuid().ToString();
 
