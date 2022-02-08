4c7d16288c46f00766fa1dd5be40d21f84cb28c0
Jason Bock <jasonbock1@rocketmortgage.com>
2022-02-08 09:23:50 -0600

    Upgraded/changed the example project to .NET 6

---
+++ Examples/Middleware.NET60/WeatherForecast.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Middleware.NET60;
+
+public sealed class WeatherForecast
+{
+   public static readonly string[] Summaries = new[]
+   {
+      "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+   };
+
+   public DateTime Date { get; init; }
+
+   public int TemperatureC { get; init; }
+
+   public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+   public string? Summary { get; init; }
+}
